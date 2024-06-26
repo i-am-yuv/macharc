774774c7f6ad394e2e93da23aaf1be87f576bbdc
yuvraj <yuvraj.b@splenta.com>
2024-06-26 11:26:56 +0530

    media manager static page created:

--- projects/studio/src/app/data-form/media-manager/media-manager.component.ts
+++ projects/studio/src/app/data-form/media-manager/media-manager.component.ts
@@ -1,3 +1,4 @@
+import { HttpClient, HttpEventType } from '@angular/common/http';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,34 @@ import { Component } from '@angular/core';
 })
 export class MediaManagerComponent {
 
+  times: number[] = [1, 2, 3, 4,5,6,7,8,9,10];
+
+  constructor(private http: HttpClient) {}
+
+  onFileSelected(event: any) {
+    const file: File = event.target.files[0];
+    if (file) {
+      const formData = new FormData();
+      formData.append('file', file);
+      console.log(file);
+      // this.http.post('YOUR_BACKEND_API_URL', formData, {
+      //   reportProgress: true,
+      //   observe: 'events'
+      // }).subscribe(event => {
+      //   if (event.type === HttpEventType.UploadProgress) {
+      //     const progress = Math.round(100 * event.loaded / event.total!);
+      //     console.log(`File is ${progress}% uploaded.`);
+      //   } else if (event.type === HttpEventType.Response) {
+      //     console.log('File uploaded successfully!', event.body);
+      //   }
+      // }, error => {
+      //   console.error('Error uploading file:', error);
+      // });
+    }
+  }
+
+  triggerFileInput() {
+    const fileInput = document.getElementById('fileInput') as HTMLElement;
+    fileInput.click();
+  }
 }
