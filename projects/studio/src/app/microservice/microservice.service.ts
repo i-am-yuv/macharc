a542ee2d8e2248b3561e757c1dbf41d189887f03
Partha <partha@splenta.com>
2024-07-30 12:26:29 +0530

    Create Microservice Fix

--- projects/studio/src/app/microservice/microservice.service.ts
+++ projects/studio/src/app/microservice/microservice.service.ts
@@ -1,15 +1,12 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { GenericService } from '../utils/genericservice';
 import { lastValueFrom } from 'rxjs';
+import { GenericService } from '../utils/genericservice';
 
 @Injectable({
-  providedIn: 'root'
+  providedIn: 'root',
 })
 export class MicroserviceService extends GenericService {
-
-
-
   endpoint: string = 'micro-services';
 
   constructor(http: HttpClient) {
@@ -17,24 +14,54 @@ export class MicroserviceService extends GenericService {
   }
 
   async generateCode(ms: any) {
-    var url = this.apiurl + '/' + this.endpoint + '/generate/' + encodeURIComponent(ms.id!);
+    var url =
+      this.apiurl +
+      '/' +
+      this.endpoint +
+      '/generate/' +
+      encodeURIComponent(ms.id!);
     const res = await lastValueFrom(this.http.get<any>(url));
     return res;
   }
   async generateFrontendCode(ms: any) {
-    var url = this.apiurl + '/' + this.endpoint + '/generateFrontend/' + encodeURIComponent(ms.id!);
+    var url =
+      this.apiurl +
+      '/' +
+      this.endpoint +
+      '/generateFrontend/' +
+      encodeURIComponent(ms.id!);
     const res = await lastValueFrom(this.http.get<any>(url));
     return res;
   }
   async linkDs(msId: string, dsId: string) {
-    var url = this.apiurl + '/' + this.endpoint + '/addDataSource/' + encodeURIComponent(msId) + '/' + encodeURIComponent(dsId!);
+    var url =
+      this.apiurl +
+      '/' +
+      this.endpoint +
+      '/addDataSource/' +
+      encodeURIComponent(msId) +
+      '/' +
+      encodeURIComponent(dsId!);
     const res = await lastValueFrom(this.http.get<any>(url));
     return res;
   }
 
   async unLinkDs(msId: string, dsId: string) {
-    var url = this.apiurl + '/' + this.endpoint + '/removeDataSource/' + encodeURIComponent(msId) + '/' + encodeURIComponent(dsId!);
+    var url =
+      this.apiurl +
+      '/' +
+      this.endpoint +
+      '/removeDataSource/' +
+      encodeURIComponent(msId) +
+      '/' +
+      encodeURIComponent(dsId!);
     const res = await lastValueFrom(this.http.get<any>(url));
+    return res;
+  }
+
+  async saveMicroservice(ms: any) {
+    var url = this.apiurl + '/' + this.endpoint + '/create';
+    const res = await lastValueFrom(this.http.post<any>(url, ms));
     return res;
   }
 }
--- projects/studio/src/app/microservice/ms-form/ms-form.component.ts
+++ projects/studio/src/app/microservice/ms-form/ms-form.component.ts
@@ -1,43 +1,51 @@
 import { Component } from '@angular/core';
-import { GenericComponent } from '../../utils/genericcomponent';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
-import { MessageService } from '@splenta/vezo/src/public-api';
-import { MicroserviceService } from '../microservice.service';
 import { ActivatedRoute, Router } from '@angular/router';
+import { MessageService } from '@splenta/vezo/src/public-api';
 import { ProjectService } from '../../project/project.service';
+import { GenericComponent } from '../../utils/genericcomponent';
+import { MicroserviceService } from '../microservice.service';
 
 @Component({
   selector: 'app-ms-form',
   templateUrl: './ms-form.component.html',
-  styleUrls: ['./ms-form.component.scss']
+  styleUrls: ['./ms-form.component.scss'],
 })
 export class MsFormComponent extends GenericComponent {
-
   override form!: FormGroup<any>;
   override data: any[] = [];
   override componentName: string = 'Microservice Form';
   msId: string | null;
   packaging: any[] = ['Jar', 'War'];
   projects: any[] = [];
+  loading: boolean = false;
 
   constructor(
     private fb: FormBuilder,
     msService: MicroserviceService,
     messageService: MessageService,
     private projectService: ProjectService,
-    private router : Router ,
-    private route: ActivatedRoute) {
+    private router: Router,
+    private route: ActivatedRoute,
+    private micrService: MicroserviceService
+  ) {
     super(msService, messageService);
     this.msId = this.route.snapshot.paramMap.get('id');
     this.form = this.fb.group({
       id: '',
-      microServiceCode: ['', [Validators.required, Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/)]],
-      microServiceName: ['', [Validators.required, Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/)]],
+      microServiceCode: [
+        '',
+        [Validators.required, Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/)],
+      ],
+      microServiceName: [
+        '',
+        [Validators.required, Validators.pattern(/^(\s+\S+\s*)*(?!\s).*$/)],
+      ],
       packageName: [''],
       packaging: ['Jar'],
       portNumber: ['', [Validators.required]],
-      project: ['']
-    })
+      project: [''],
+    });
   }
 
   ngOnInit(): void {
@@ -46,9 +54,33 @@ export class MsFormComponent extends GenericComponent {
     }
     this.projectService.getAllData().then((res: any) => {
       this.projects = res.content;
-    })
+    });
   }
   loadData(res: any): void {
     this.form.patchValue({ ...res });
+  }
+
+  saveMicroService() {
+    this.loading = true;
+    this.micrService
+      .saveMicroservice(this.form.value)
+      .then((res: any) => {
+        if (res) {
+          this.messageService.add({
+            severity: 'success',
+            summary: 'Generated',
+            detail: 'Microservice created',
+          });
+        }
+        this.loading = false;
+      })
+      .catch((e) => {
+        this.loading = false;
+        this.messageService.add({
+          severity: 'error',
+          summary: 'Error Generating',
+          detail: 'Sorry, there was an error generating the microservice',
+        });
+      });
   }
 }
