5c110b1fdd8a2951942952ed26c22f399165f88e
yuvraj <yuvraj.b@splenta.com>
2024-08-21 17:06:12 +0530

    actions copy pasted

--- projects/studio/src/app/actions/action.ts
+++ projects/studio/src/app/actions/action.ts
@@ -1,8 +1,12 @@
+import { MicroService } from "../microservice/microservice";
+
 export class Actions {
     id ?: string;
     actionName ?: string ;
     actionType ?: string ;
+    taskDefinition ?: string ;
     actionTasks ?: ActionTask[] ;
+    microService  ?: MicroService
 }
 
 export class ActionTask {
