9fcda3546c6edf948ee103d38f2ef1121f1b9147
KirpalR-ABCD <kirpal.ravi-v@adityabirlacapital.com>
2024-08-27 18:00:24 +0530

    Docker Configuration Added for Building

---
+++ projects/studio/src/environments/environment.docker.ts
@@ -0,0 +1,17 @@
+export const environment = {
+    production: true,
+    // apiurl: 'http://localhost:8222/api',
+    apiurl: 'https://apistaging1.macharc.com/api',
+    gitServerUrl: 'https://repostaging1.macharc.com',
+    llmUrl: 'http://localhost:11434',
+    // apiurl: 'https://studio.macharc.com/api',
+    firebaseConfig: {
+        apiKey: "AIzaSyAP2rqd6wemU197DAVT6eFCFcuEzTm0dX0",
+        authDomain: "macharc-dev.firebaseapp.com",
+        projectId: "macharc-dev",
+        storageBucket: "macharc-dev.appspot.com",
+        messagingSenderId: "846154939781",
+        appId: "1:846154939781:web:fb45d62680d8fb5f3618ec"
+    },
+    webTerminal: 'ws://localhost:8686/rsocket'
+};
